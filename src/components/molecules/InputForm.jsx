a04827e77b7d6f6ac2417ae85eafc9179729a782
K-jisu <wltn7star@naver.com>
2025-01-22 19:53:10 +0900

    ✨ update : 버튼 수정, input value 값 추가

--- src/components/molecules/InputForm.jsx
+++ src/components/molecules/InputForm.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "../atoms/Button";
 import Input from "../atoms/Input";
 
-const InputForm = () => {
+const InputForm = ({ handleInputChange, handleSubmit, data }) => {
   return (
     <form
       onSubmit={handleSubmit}
@@ -12,27 +12,32 @@ const InputForm = () => {
         id={"country"}
         label={"국가명"}
         type={"text"}
+        value={data.country}
         handler={handleInputChange}
       />
       <Input
         id={"gold"}
         label={"금메달"}
         type={"number"}
+        value={data.gold}
         handler={handleInputChange}
       />
       <Input
         id={"silver"}
         label={"은메달"}
         type={"number"}
+        value={data.silver}
         handler={handleInputChange}
       />
       <Input
         id={"bronze"}
         label={"동메달"}
         type={"number"}
+        value={data.bronze}
         handler={handleInputChange}
       />
-      <Button></Button>
+      <Button type={"submit"} content={"국가 추가"} name={"addBtn"} />
+      <Button type={"submit"} content={"업데이트"} name={"update"} />
     </form>
   );
 };
