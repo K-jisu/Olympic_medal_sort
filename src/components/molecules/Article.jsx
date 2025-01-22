81b4539c92f329f3b07a28de759c2b1bd67cb5a7
K-jisu <wltn7star@naver.com>
2025-01-22 19:52:17 +0900

    ✨ update : 더미데이터 제거 후 데이터 연결

--- src/components/molecules/Article.jsx
+++ src/components/molecules/Article.jsx
@@ -1,32 +1,28 @@
 import React from "react";
 import Button from "../atoms/Button";
 
-const Article = () => {
-  const submitList = [{
-    id:1,
-    country:"캐나다",
-    gold : 2,
-    silver : 4,
-    bronze : 10,
-  }, 
-  {
-    id:2,
-    country:"미국",
-    gold : 2,
-    silver : 4,
-    bronze : 10,
-  }
-  ]
+const Article = ({ submitList, handleDelete }) => {
   return (
     <>
       {submitList.map((item) => {
         return (
-          <article key={item.id}>
+          <article
+            key={item.country}
+            style={{ display: "flex", flexDirection: "row", gap: "20px" }}
+          >
             <p>{item.country}</p>
             <p>{item.gold}</p>
             <p>{item.silver}</p>
             <p>{item.bronze}</p>
-            <Button type = {"button"} content= {"삭제"}/>
+            <p>
+              {Number(item.gold) + Number(item.silver) + Number(item.bronze)}
+            </p>
+            <Button
+              type={"button"}
+              content={"삭제"}
+              name={item.country}
+              event={handleDelete}
+            />
           </article>
         );
       })}
