0bfa99ea0518d720cc2d45eaa4b7a53547f8d8d5
K-jisu <wltn7star@naver.com>
2025-01-22 19:54:05 +0900

    ✨ update : App.jsx 에서 Body.jsx로 함수 이동

--- src/components/organism/Body.jsx
+++ src/components/organism/Body.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import Article from '../molecules/Article';
+import React from "react";
+import Article from "../molecules/Article";
 
-
-const Body = () => {
+const Body = ({ submitList, setSubmitList }) => {
+  // 목록 삭제
+  const handleDelete = (e) => {
+    const filteredData = submitList.filter(
+      (item) => item.country !== e.nativeEvent.target.name
+    );
+    setSubmitList(filteredData);
+  };
   return (
     <>
       <section>
-        <div>
+        <div style={{display:"flex", gap:"20px"}}>
           <h5>국가명</h5>
           <h5>금메달</h5>
           <h5>은메달</h5>
           <h5>동메달</h5>
+          <h5>총 메달 수</h5>
           <h5>액션</h5>
         </div>
-        <Article/>
+        <Article submitList={submitList} handleDelete={handleDelete} />
       </section>
     </>
   );
