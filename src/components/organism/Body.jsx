68352277c1d6e33765444a6d0c6de1254574aabb
K-jisu <wltn7star@naver.com>
2025-01-24 15:49:30 +0900

    ✨ update: Article 컴포넌트 합체

--- src/components/organism/Body.jsx
+++ src/components/organism/Body.jsx
@@ -1,7 +1,10 @@
-import Article from "../molecules/Article";
-import { setLocalStorage } from "../utils/localStorage";
+import Button from "../atoms/Button";
+import { getLocalStorage, setLocalStorage } from "../utils/localStorage";
+import "../styles/body.css";
 
 const Body = ({ submitList, setSubmitList }) => {
+  const localStorageList = getLocalStorage("list");
+
   // 목록 삭제
   const handleDelete = (e) => {
     const filteredData = submitList.filter(
@@ -14,7 +17,7 @@ const Body = ({ submitList, setSubmitList }) => {
   return (
     <main>
       <section>
-        <div style={{ display: "flex", gap: "20px" }}>
+        <div className="item">
           <h5>국가명</h5>
           <h5>금메달</h5>
           <h5>은메달</h5>
@@ -22,7 +25,27 @@ const Body = ({ submitList, setSubmitList }) => {
           <h5>총 메달 수</h5>
           <h5>액션</h5>
         </div>
-        <Article submitList={submitList} handleDelete={handleDelete} />
+        {localStorageList.map((item) => {
+          return (
+            <article
+              key={item.country}
+            >
+              <p>{item.country}</p>
+              <p>{item.gold}</p>
+              <p>{item.silver}</p>
+              <p>{item.bronze}</p>
+              <p>
+                {Number(item.gold) + Number(item.silver) + Number(item.bronze)}
+              </p>
+              <Button
+                type={"button"}
+                content={"삭제"}
+                name={item.country}
+                onClick={handleDelete}
+              />
+            </article>
+          );
+        })}
       </section>
     </main>
   );
